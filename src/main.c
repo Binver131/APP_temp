4c459f8def3c609d4a1dadc8d6093a2ed7f134fa
汪文轩 <wangwengx.1997@163.com>
2024-04-07 18:14:01 +0800

    🐳 chore(main): 主模块添加arch支持

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-
+#include "main_arch.h"
 #include"RTree/GraphicsDataStructures.h"
 #include"refs.h"
 #include"time.h"
@@ -7,9 +7,7 @@ RApp *currentAPP;
 Gcomponent *buttonRef;
 void initGraphic()
 {
-    printf("initGraphic\n");
     RTree = initRTtree();
-
     #define REF(x)  \
             init_##x();
 
@@ -39,29 +37,27 @@ void initGraphic()
     RWindow *window_ptr = addWindow(window, currentAPP);
     RLayer *layer_ptr = insertLayer(layer, window_ptr);
 
-    Gcomponent *button;
-    DefLayerGraphic(layer_ptr)
-        Component_Group(button,2)
-    EndLayerGraphic(layer_ptr)
-  for(int i = 0; i < 2; i ++)
-  {
-    DefComponentGroup(button, i, Reference)
-            PrimitiveAttr
-            {
-                .b_visible = true,
-                .s_origin = {window_ptr->width/2, window_ptr->height/2 - i * 60},
-                .f_angle = 0,
-                .p_source = pushbutton
-            }
-            AppearanceAttr
-            {
-                .b_visiable = true
-            }
+    GARCH
 
-    EndComponentGroup(button, i)
-  }
 
-    buttonRef = button;
+    DefComponet(HelloWorldlabel,Text)
+        PrimitiveAttr
+        {
+            .s_position = {window.width/2, window.height/2},
+            .sc_text = str("Hello World"),
+            .i_maxLegth = 32,
+            .sizeX = 46,
+            .sizeY = 23,
+            0
+        }
+        AppearanceAttr
+        {
+            .b_visiable = true,
+            .e_lineColor = 14,
+            .e_TextureAlignment = 0x13,
+            .i_font = 1,
+        }
+    EndComponent(HelloWorldlabel)
 }
 
 clock_t start,now;
@@ -73,18 +69,9 @@ void main(){
         while (!currentAPP->endRender){}
         now = clock();
         double time = (double)(now - start)/CLOCKS_PER_SEC;
-        #if 0
-        Vertex pos ={sin(time)*840+840, GET_PARM(compass_pos).y};
 
-        //SET_PARM(compass_pos, pos);
-        SET_PARM(compass_HEAD, sin(0.1*time)*360);
-        SET_PARM(HDG, str("%3.0f", GET_PARM(compass_HEAD)));
-        #endif
-      struct pushbutton_Graphic_Parm_s parm = {.High_light = true, .string = str("Button1")};
-        set_pushbutton_parm(parm, buttonRef);
-        parm.High_light = false;
-        parm.string = str("Button2");
-        set_pushbutton_parm(parm, buttonRef + 1);
+        /*main logic here*/
+
         currentAPP->endRender = false;
         currentAPP->message_ptr = 0;
     }
